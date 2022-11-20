09df3b0c6e2711d2b3f43f2981a6e148c998b34c
Pratik R Raval <111408819+rpratik15@users.noreply.github.com>
2022-11-20 19:44:10 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import RenderPara from "./RenderPara";
 
 function App() {
   return (
     <div id="main">
-      // Do not alter the main div
+     <RenderPara/>
     </div>
   );
 }
---
+++ src/components/RenderPara.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react'
+
+function RenderPara() {
+
+    const [show,setShow]=useState(false)
+    const showHide=()=>
+    {
+        setShow((prevState) => !prevState )
+        
+    }
+    
+  return (
+    <>
+        <button id="click" onClick={showHide}>Click</button>
+
+        {show && ( <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}
+      
+        </>
+  )
+}
+
+export default RenderPara
